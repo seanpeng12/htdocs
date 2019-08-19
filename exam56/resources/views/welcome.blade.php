c1897e187f6a6029325690352d6ddb4aab7b437f
seanpeng12 <seanpeng12@gmail.com>
2019-08-19 13:11:40 +0800

    backpack安裝

--- exam56/resources/views/welcome.blade.php
+++ exam56/resources/views/welcome.blade.php
@@ -8,3 +8,12 @@
     --}}
     <h1>隨機題庫測試系統</h1>
 @endsection
+
+@section('my_menu')
+    <li>
+        <a class="nav-link" href="/home">
+            我的選項
+        </a>
+    </li>
+    @parent
+@stop
